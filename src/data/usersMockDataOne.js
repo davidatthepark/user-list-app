00e74f29d2c29c3313ca3015f6d13c3188143a03
David Park <dpark4657@gmail.com>
2026-04-10 16:09:14 -0700

    Add mock users data

---
+++ src/data/usersMockDataOne.js
@@ -0,0 +1,26 @@
+const usersMockDataOne = [
+  {
+    firstName: "John",
+    lastName: "Schmitt",
+    email: "johnschmitt@example.com",
+    status: "active",
+    created: "Thu Jan 05 2020 12:28:09 GMT-0500 (Eastern Standard Time)",
+    updated: "Thu Jan 05 2020 13:33:29 GMT-0500 (Eastern Standard Time)",
+  },
+  {
+    firstName: "Beth",
+    lastName: "Forest",
+    email: "bethforest@example.com",
+    status: "inactive",
+    created: "Fri June 06 2021 15:01:07 GMT-0500 (Eastern Standard Time)",
+    updated: "Fri June 06 2022 15:01:07 GMT-0500 (Eastern Standard Time)",
+  },
+  {
+    firstName: "Edward",
+    lastName: "Jones",
+    email: "edwardjones@example.com",
+    status: "disabled",
+    created: "Tue Sept 24 2022 16:45:11 GMT-0500 (Eastern Standard Time)",
+    updated: "Mon Nov 03 2022 11:12:45 GMT-0500 (Eastern Standard Time)",
+  },
+];
---
+++ src/data/usersMockDataTwo.js
@@ -0,0 +1,34 @@
+const usersMockDataTwo = [
+  {
+    firstName: "Nina",
+    lastName: "Schmitt",
+    email: "ninaschmitt@example.com",
+    status: "active",
+    created: "Thu Jan 05 2020 15:28:09 GMT-0800 (Pacific Standard Time)",
+    updated: "Thu Jan 05 2020 14:33:29 GMT-0800 (Pacific Standard Time)",
+  },
+  {
+    firstName: "Forest",
+    lastName: "Gump",
+    email: "forestgump@example.com",
+    status: "active",
+    created: "Fri June 06 2021 16:01:07 GMT-0800 (Pacific Standard Time)",
+    updated: "Fri June 06 2022 19:01:07 GMT-0800 (Pacific Standard Time)",
+  },
+  {
+    firstName: "Wizard",
+    lastName: "Oz",
+    email: "wizardoz@example.com",
+    status: "inactive",
+    created: "Tue Sept 24 2022 13:45:11 GMT-0800 (Pacific Standard Time)",
+    updated: "Mon Nov 03 2022 12:12:45 GMT-0800 (Pacific Standard Time)",
+  },
+  {
+    firstName: "Keanu",
+    lastName: "Reeves",
+    email: "theone@matrix.com",
+    status: "disabled",
+    created: "Mon Sept 23 2022 13:45:11 GMT-0800 (Pacific Standard Time)",
+    updated: "Mon Nov 03 2022 12:12:45 GMT-0800 (Pacific Standard Time)",
+  },
+];
